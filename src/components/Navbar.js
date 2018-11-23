a6ac2f0ae330bee4fafafa8cf0e8c60cc4162b5d
Chris Fung <aergonaut@gmail.com>
2018-11-22 18:43:09 -0800

    add code highlighting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,36 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/coupa-logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import github from "../img/github-icon.svg";
+import logo from "../img/coupa-logo.svg";
 
 const Navbar = () => (
-  <div className="columns is-centered">
-    <div className="column is-two-thirds">
-      <nav className="navbar is-transparent">
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item">
-              <h1>Coupa Engineering</h1>
-            </Link>
+  <div className="container">
+    <div className="columns is-centered">
+      <div className="column is-two-thirds">
+        <nav className="navbar is-transparent">
+          <div className="container">
+            <div className="navbar-brand">
+              <Link to="/" className="navbar-item">
+                <h1>Coupa Engineering</h1>
+              </Link>
+            </div>
+            <div className="navbar-start" />
+            <div className="navbar-end">
+              <Link className="navbar-item" to="/tags/brewing">
+                Brewing
+              </Link>
+              <Link className="navbar-item" to="/tags/tasting">
+                Tasting
+              </Link>
+              <Link className="navbar-item" to="/about">
+                About
+              </Link>
+            </div>
           </div>
-          <div className="navbar-start">
-          </div>
-          <div className="navbar-end">
-            <Link className="navbar-item" to="/tags/brewing">
-              Brewing
-            </Link>
-            <Link className="navbar-item" to="/tags/tasting">
-              Tasting
-            </Link>
-            <Link className="navbar-item" to="/about">
-              About
-            </Link>
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   </div>
-)
+);
 
-export default Navbar
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <Layout>
@@ -14,56 +14,55 @@ export default class IndexPage extends React.Component {
           <div className="container">
             <div className="columns is-centered">
               <div className="column is-two-thirds">
-                <div className="content">
-                  <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-                </div>
-                {posts
-                  .map(({ node: post }) => (
-                    <div
-                      className="content"
-                      style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                      key={post.id}
-                    >
-                      <h1 className="title">
-                        <Link className="has-text-primary" to={post.fields.slug}>
-                          {post.frontmatter.title}
-                        </Link>
-                      </h1>
-                      <p>
-                        <small>{post.frontmatter.date}</small>
-                      </p>
-                      <p>
-                        {post.excerpt}
-                        <br />
-                        <br />
-                        <Link className="button is-small" to={post.fields.slug}>
-                          Keep Reading →
-                        </Link>
-                      </p>
-                    </div>
-                  ))}
+                {posts.map(({ node: post }) => (
+                  <div
+                    className="box content"
+                    style={{ padding: "2rem 3rem" }}
+                    key={post.id}
+                  >
+                    <h1 className="title">
+                      <Link
+                        className="has-text-black-bis"
+                        to={post.fields.slug}
+                      >
+                        {post.frontmatter.title}
+                      </Link>
+                    </h1>
+                    <p>
+                      <small>{post.frontmatter.date}</small>
+                    </p>
+                    <p>
+                      {post.excerpt}
+                      <br />
+                      <br />
+                      <Link className="button" to={post.fields.slug}>
+                        Keep Reading →
+                      </Link>
+                    </p>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+      edges: PropTypes.array
+    })
+  })
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
     ) {
       edges {
         node {
@@ -81,4 +80,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
